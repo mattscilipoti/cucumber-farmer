e0ae5294df1e2dfce3bbf49da496a2aad2ee8a7f
Matt Scilipoti <matt@scilipoti.name>
2010-04-26 16:34:49 -0400

    copy/paste pending step_defs

---
+++ test_app/features/step_definitions/web_steps.rb
@@ -0,0 +1,7 @@
+When /^I visit the home page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "([^\"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
